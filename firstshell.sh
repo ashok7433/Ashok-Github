af37be99331f229745b9974185becb8b367e8fd1
Ashok <ashok@Ashok-PC.Ashok.com>
2024-10-21 12:56:35 +0530

    Commited firstsell.sh v2

--- firstshell.sh
+++ firstshell.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "MY NAME IS ASHOK"
+
+GIT
+------------
+git init -- git reposirty is created .git hidden folder git will track by using .git
+git 
+
+ls .git -- every thing git will takced as objects
+HEAD config hooks objects refs
+
+git add -- 
+git diff -- tell exact changes made
+add . (or) git add filename (or) git add --all
+git commit -m "Added firstshellscript.sh file"
+
+
